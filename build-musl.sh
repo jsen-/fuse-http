d93cc575fdea984ead38692e9a41e8aef0e346b3
jsen- <max.enhanced@gmail.com>
2022-05-08 16:36:47 +0200

    remove unnecessary env vars from buildscript

--- build-musl.sh
+++ build-musl.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-PKG_CONFIG_ALLOW_CROSS=1 \
-RUSTFLAGS='-C link-arg=-lgcc_eh -C link-arg=-lgcc -C target-feature=+crt-static' \
 cargo +nightly build --release \
   -Z build-std=core,std,alloc,panic_abort \
   -Z build-std-features= \
